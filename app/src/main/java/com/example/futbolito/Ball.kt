babd832cc2ec9cb983f3435062ad177a66468de2
Aurora <auro.prz1809@gmail.com>
2023-03-22 20:16:55 -0600

    Agrega rebote

--- app/src/main/java/com/example/futbolito/Ball.kt
+++ app/src/main/java/com/example/futbolito/Ball.kt
@@ -1,6 +1,5 @@
 package com.example.futbolito
 
-import android.R
 import android.content.Context
 import android.graphics.*
 import android.hardware.Sensor
@@ -8,13 +7,10 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Build
-import android.os.Vibrator
-import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import java.lang.Float.min
 
 
 @RequiresApi(Build.VERSION_CODES.R)
@@ -22,17 +18,23 @@ class Ball(context: Context?) : View(context), SensorEventListener {
     var size: Float = 80F
     var x1 : Float = 0F
     var y1 : Float = 0F
-    var z1 : Float = 0F
+    var vx1 : Float = 0F
+    var vy1 : Float = 0F
+    var gx1 : Float = 0F
+    var gy1 : Float = 0F
+    var lx1 : Float = 0F
+    var ly1 : Float = 0F
+    var direccionX = -1f;
+    var direccionY = 1f;
     var flag: Boolean = false
     var valido1 : Boolean = false
     var valido2 : Boolean = false
-    lateinit var bmpCancha : Bitmap
-    lateinit  var scaledBmpCancha : Bitmap
-    lateinit var bmpBalon : Bitmap
-    lateinit var scaledBmpBalon : Bitmap
+    var bmpCancha : Bitmap
+    var scaledBmpCancha : Bitmap
+    var bmpBalon : Bitmap
+    var scaledBmpBalon : Bitmap
 
     init{
-
         var sm = context?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         var acelerometro = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         sm.registerListener(this, acelerometro, SensorManager.SENSOR_DELAY_GAME)
@@ -49,27 +51,47 @@ class Ball(context: Context?) : View(context), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        x1 -= event?.values?.get(0)!!
-        y1 += event?.values?.get(1)!!
-        z1 = event?.values?.get(2)!!
-        Log.d("coordenadas",x1.toString()+" "+y1.toString())
+        val alpha: Float = 0.8f
+
+        // Isolate the force of gravity with the low-pass filter.
+        gx1 = alpha * gx1 + (1 - alpha) * event!!.values[0]
+        gy1 = alpha * gy1 + (1 - alpha) * event!!.values[1]
+
+        // Remove the gravity contribution with the high-pass filter.
+        lx1 += event.values[0] - gx1
+        ly1 += event.values[1] - gy1
+
+        vx1 = event?.values?.get(0)!!
+        vy1 = event?.values?.get(1)!!
+        actualizar()
+    }
+
+    private fun actualizar() {
+        x1 += direccionX*lx1
+        y1 += direccionY*ly1
 
         //COLISIONES
         if(x1>=(width-size)){
-            x1 = width- size
-        }else if(x1<size){
-            x1 = size
+            direccionX *= -1f
+        }else if(x1<=0.toFloat()){
+            direccionX *= -1f
         }
+        /*else if(x1>(width*0.4) && x1<width-(width*0.4)){
+            direccionX = -1f
+        }*/
 
         if(y1>=(height-size)){
-            y1 = height- size
+            direccionY *= -1f
             valido1 = false
             valido2 = false
-        }else if(y1<size){
-            y1 = size
+        }else if(y1<=0.toFloat()){
+            direccionY *= -1f ;
             valido1 = false
             valido2 = false
         }
+        /*else if(y1>(height*0.4) && y1<height-(height*0.4)){
+            direccionY = 1f
+        }*/
 
         if(y1>=size*3){
             valido1 = true
@@ -77,9 +99,6 @@ class Ball(context: Context?) : View(context), SensorEventListener {
         if( y1<=(height-(height*0.08))){
             valido2 = true
         }
-
-
-
         //PORTERIA ARRIBA
         if(x1>=(width/2)-100.toFloat() && x1<=(width/2)+50.toFloat() && y1>size+50 && y1<=size+80 && valido1){
             Toast.makeText(context,"GOL EQUIPO 1!",Toast.LENGTH_SHORT).show()
@@ -92,14 +111,7 @@ class Ball(context: Context?) : View(context), SensorEventListener {
             x1 = (width/2).toFloat()
             y1 = (height/2-(height*0.08)).toFloat()
         }
-
-
-
-
         invalidate()
-
-
-
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
